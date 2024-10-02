d0ea33947aa333b2ba8193dde91b67b3da308e1c
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-10-02 11:00:02 +0300

    add content to landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,9 +2,21 @@ const Home = () => {
   return (
     <div className="">
       <main className="">
-        <h1>Serhat Başesgioğlu</h1>
-        <p>Welcome to the my portfolio website!!</p>
-        <p>This page will be filled out with content later on!</p>
+        <section className="mb-8">
+          <h2 className="text-2xl font-bold">About me</h2>
+          <p>Hi, my name is Serhat, I am a software developer!</p>
+        </section>
+        <section className="my-8">
+          <h2 className="text-2xl font-bold">Projects</h2>
+          <p>I made a couple of projects which can be seen below!</p>
+          <div>
+            <p>Plus30</p>
+            <p>Portfolio</p>
+            <p>MicroserviceTest</p>
+            <p>Blog</p>
+            <p>Other Stuff</p>
+          </div>
+        </section>
       </main>
     </div>
   );
