41b9afaa6b06f5b7a82efa299d339e5e090350b1
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-10-02 10:48:58 +0300

    add initial template for footer and header

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,16 @@
 const Footer = () => {
   return (
-    <div>
-      <p>This is the footer component</p>
-    </div>
+    <footer className="bg-gray-300">
+      <div className="flex justify-between items-center">
+        <p>2024 Serhat Başesgioğlu. All right reserved.</p>
+        <div className="flex space-x-4">
+          <p>Twitter</p>
+          <p>Github</p>
+          <p>Linkedin</p>
+          <p>Discord</p>
+        </div>
+      </div>
+    </footer>
   );
 };
 export default Footer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,15 @@
 const Header = () => {
   return (
-    <div>
-      <p>This is the header component</p>
-    </div>
+    <header className="bg-gray-200 p-4">
+      <nav className="flex justify-between items-center">
+        <p>This is the logo</p>
+        <div className="flex space-x-4">
+          <p>Portfolio</p>
+          <p>Blog</p>
+          <p>Reach me</p>
+        </div>
+      </nav>
+    </header>
   );
 };
 export default Header;
