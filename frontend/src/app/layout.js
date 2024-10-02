6dae3f8ce68b5435e4b082c3ca18acc6bbc495c2
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-10-02 12:42:16 +0300

    add metadata, basic sitemap and notes

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,13 +1,24 @@
+import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default: "Serhat Başesgioğlu",
+    template: "%s - Serhat Başesgioğlu",
+  },
+  description: "You can read my portfolio from here!",
+};
+
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
-        <div className="flex-1">{children}</div>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
