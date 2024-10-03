4e3b1b784e3a2ccf154bcc3b04edad33f4328026
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-10-03 13:57:01 +0300

    move main tag into rootlayout

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,23 +1,23 @@
+import ProjectList from "@/components/ProjectList";
+
 const Home = () => {
   return (
     <div className="">
-      <main className="">
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold">About me</h2>
-          <p>Hi, my name is Serhat, I am a software developer!</p>
-        </section>
-        <section className="my-8">
-          <h2 className="text-2xl font-bold">Projects</h2>
-          <p>I made a couple of projects which can be seen below!</p>
-          <div>
-            <p>Plus30</p>
-            <p>Portfolio</p>
-            <p>MicroserviceTest</p>
-            <p>Blog</p>
-            <p>Other Stuff</p>
-          </div>
-        </section>
-      </main>
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold">About me</h2>
+        <p>Hi, my name is Serhat, I am a software developer!</p>
+      </section>
+      <section className="my-8">
+        <h2 className="text-2xl font-bold">Projects</h2>
+        <p>I made a couple of projects which can be seen below!</p>
+        <div>
+          <p>Plus30</p>
+          <p>Portfolio</p>
+          <p>MicroserviceTest</p>
+          <p>Blog</p>
+        </div>
+      </section>
+      <ProjectList />
     </div>
   );
 };
